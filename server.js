b56d20ff825a4ec084ad2c1eb3eab82772e572e2
Narubed Kaewserd <40911777+Narubed@users.noreply.github.com>
2022-04-06 13:25:25 +0700

    chang port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
-let PORT = process.env.PORT || 8080;
+let PORT = process.env.PORT || 8000;
 require("dotenv").config();
 
 const cors = require("cors");
